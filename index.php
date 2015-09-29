fbe14dece98121194657ecb6232bb350ea0c912b
joakimsigvald <joakim.sigvald@gmail.com>
2015-09-29 14:28:02 +0200

    Moved special handling of add forum post to execute_scripts and cleaned up Index.php

--- index.php
+++ index.php
@@ -1,38 +1,18 @@
 <?php 
-
 include("helpers/init.inc");
-
-if($_GET['type']=="forum" && $_GET['action']=="show_posts"){
-	include("execute_scripts/forum/sort_posts_and_topics.inc");
-}
-elseif($_GET['type']=="forum" && $_GET['action']=="delete"){
-	include("execute_scripts/forum/delete_post.inc");
-}
-
-if($_POST){
-	include("execute_scripts/execute_scripts.inc");
-}
-
+include("execute_scripts/execute_scripts.inc");
 ?>
 <html><head>
-
 <?php
+include("helpers/load_css.inc");
+include("helpers/load_js.inc");
 if(isset($_GET['login']) || isset($_GET['logout']) || (!isset($_SESSION['id']) && isset($_COOKIE['user_token']) && isset($_COOKIE['user_id']))){
 	include("helpers/login_logout.inc");
 }
 ?>
-
-<?php include("helpers/load_css.inc"); ?>
-<?php include("helpers/load_js.inc"); ?>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <link rel="shortcut icon" href="images/favicon.ico">
-
 <title>DD - Nya versionen</title>
-
-<script>
-
-</script>
-
 </head>
 
 <body>
