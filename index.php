3dacf1b82f13cc6c392a26e1e23621aa22efb225
joakimsigvald <joakim.sigvald@gmail.com>
2015-10-19 02:13:54 +0200

    Made aquirement of user codes more robust + some refactoring

--- index.php
+++ index.php
@@ -1,30 +1,20 @@
-<?php 
-include("helpers/init.inc");
-navigation_helpers::execute_action_script();
-?>
-<html><head>
-<?php
-include("helpers/load_css.inc");
-include("helpers/load_js.inc");
-if(isset($_GET['login']) || isset($_GET['logout']) || (!isset($_SESSION['id']) && isset($_COOKIE['user_token']) && isset($_COOKIE['user_id']))){
-	include("helpers/login_logout.inc");
-}
-?>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<link rel="shortcut icon" href="images/favicon.ico">
-<title>DD - Nya versionen</title>
-</head>
-
-<body>
-
-	<?php
-	if(isset($_SESSION['id'])){
-		require("index_logged_in.inc");
-	}
-	else{
-		require("index_logged_out.inc");
-	}
-	?>
-
-</body>
+<html>
+	<head>
+		<?php
+			include("helpers/init.inc");
+			include("helpers/load_css.inc");
+			include("helpers/load_js.inc");
+			include("helpers/session.inc");
+		?>
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+		<link rel="shortcut icon" href="images/favicon.ico">
+		<title>DD - Nya versionen</title>
+	</head>
+	<body>
+		<?php
+			navigation_helpers::execute_action_script();
+			session::login_logout();
+			require(isset($_SESSION['id']) ? "index_logged_in.inc" : "index_logged_out.inc");
+		?>
+	</body>
 </html>
