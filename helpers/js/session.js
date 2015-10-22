7cb9051483023ce1ec141c9aff2ad52b0b55f9d8
joakimsigvald <joakim.sigvald@gmail.com>
2015-10-22 20:59:04 +0200

    Återställt säkerheten i hämtning av user_code på klienten
    https://trello.com/c/58fZnOkD/105-sessionstorage-fungerar-inte-om-man-oppnar-en-ny-flik

--- helpers/js/session.js
+++ helpers/js/session.js
@@ -1,28 +1,21 @@
 function ensureUserCodes(url, user_id, temp_code) {
 	if (sessionStorage.active_user_code ? false : true)
-		updateUserCodes(url, user_id, temp_code);
-}
-
-function updateUserCodes(url, user_id, temp_code) {
-	make_ajax_call(url + "receive_temp_code.php?user_id=" + user_id + "&temp_code=" + temp_code,
-		function(response) {
-			if(response == 'success')
-				retrieveUserCodes(url, user_id, temp_code);
-			else 
-				alert('failed to retrieve user codes from anonymization server. Try to logout and login again. Server responded: '
-					+ response);
-		});
+		retrieveUserCodes(url, user_id, temp_code);
 }
 
 function retrieveUserCodes(url, user_id, temp_code) {
 	make_ajax_call(url + "give_out_user_codes.php?user_id=" + user_id + "&temp_code=" + temp_code,
 		function(response) {
-        	var user_codes = JSON.parse(response);
-        	sessionStorage.active_user_code = user_codes.active_user_code;
-			sessionStorage.passive_user_code1 = user_codes.passive_user_code1;
-			sessionStorage.passive_user_code2 = user_codes.passive_user_code2;
-			sessionStorage.passive_user_code3 = user_codes.passive_user_code3;
-			//alert('set user_code: ' + sessionStorage.active_user_code);
+			if (response) {
+	        	var user_codes = JSON.parse(response);
+	        	sessionStorage.active_user_code = user_codes.active_user_code;
+				sessionStorage.passive_user_code1 = user_codes.passive_user_code1;
+				sessionStorage.passive_user_code2 = user_codes.passive_user_code2;
+				sessionStorage.passive_user_code3 = user_codes.passive_user_code3;
+			}
+			else {
+				alert("Din användarsession är för gammal. Logga ut och logga in på nytt");
+			}
 	    });
 }
 
