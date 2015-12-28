20e08c8e42191e4e4995e4b5844ed164c803def5
joakimsigvald <joakim.sigvald@gmail.com>
2015-12-28 23:22:04 +0100

    https://trello.com/c/tQylIwZ9/162-nar-man-tar-bort-en-medlem-tas-inte-dess-rost-bort-i-aktiv-proposition
    remove ballot from active vote
    remove user_code from closed vote, and remove ballot without user code
    if closed vote is resumed

--- helpers/receiver_scripts_from_anon_server/remove_ballots.php
+++ helpers/receiver_scripts_from_anon_server/remove_ballots.php
@@ -2,9 +2,12 @@
 
 require("./../../init.inc");
 
-if(isset($_POST['user_code'])){
-	$ok = db_vote::remove_ballots($_POST['user_code']);
-	echo $ok
-		? "success"
-		: "error when removing ballots for user_code: " . $_POST['user_code'] . ": " . $ok;
+if(isset($_POST['active_user_code'])){
+	$ok = db_vote::remove_ballots($_POST['active_user_code']);
+	if (!$ok)
+		echo "error when removing ballots for user_code: " . $_POST['user_code'] . ": " . $ok;
+	else {
+		db_vote::remove_user_codes_from_closed_votes($_POST['passive_user_code1'],  $_POST['passive_user_code2'],  $_POST['passive_user_code3']);
+		echo "success";
+	}
 }
--- helpers/receiver_scripts_from_anon_server/remove_prop_ballots.php
+++ helpers/receiver_scripts_from_anon_server/remove_prop_ballots.php
@@ -1,9 +1,8 @@
 <?php
 
 require("./../../init.inc");
-
-if(isset($_POST['user_code'])){
-	$ok = db_prop::remove_prop_ballots($_POST['user_code']);
+if(isset($_POST['active_user_code'])){
+	$ok = db_prop::remove_prop_ballots($_POST['active_user_code']);
 	echo $ok
 		? "success"
 		: "error when removing prop ballots for user_code: " . $_POST['user_code'] . ": " . $ok;
