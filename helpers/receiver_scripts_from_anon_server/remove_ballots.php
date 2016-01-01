cc5baf762f9ee7ec62ed1b69b070f0e3474c47d4
joakimsigvald <joakim.sigvald@gmail.com>
2016-01-01 20:38:13 +0100

    remove votes and prop-suport for user when she lose constituency access

--- helpers/receiver_scripts_from_anon_server/remove_ballots.php
+++ helpers/receiver_scripts_from_anon_server/remove_ballots.php
@@ -3,10 +3,13 @@
 require("./../../init.inc");
 $data = crypt_helpers::get_signed_data_from_anon_server();
 if(isset($data['active_user_code'])){
-	$ok = db_vote::remove_ballots($data['active_user_code']);
+	$user_code = $data['active_user_code'];
+	$constituency_id = $data['constituency_id'];
+	$ok = db_vote::remove_ballots($user_code, $constituency_id);
 	if ($ok) {
-		db_vote::remove_user_codes_from_closed_votes($data['passive_user_code1'],  $data['passive_user_code2'],  $_POST['passive_user_code3']);
+		db_vote::remove_user_codes_from_closed_votes($data['passive_user_code1']
+			, $data['passive_user_code2'], $data['passive_user_code3'], $constituency_id);
 		echo "success";
 	}
-	else echo "error when removing ballots for user_code: " . $data['user_code'] . ": " . $ok;
+	else echo "failed to remove ballots for user_code: " . $user_code . " and constituency: " . $constituency_id;
 }
--- helpers/receiver_scripts_from_anon_server/remove_prop_ballots.php
+++ helpers/receiver_scripts_from_anon_server/remove_prop_ballots.php
@@ -2,8 +2,10 @@
 require("./../../init.inc");
 $data = crypt_helpers::get_signed_data_from_anon_server();
 if(isset($data['active_user_code'])){
-	$ok = db_prop::remove_prop_ballots($data['active_user_code']);
+	$user_code = $data['active_user_code'];
+	$constituency_id = $data['constituency_id'];
+	$ok = db_prop::remove_prop_ballots($user_code, $constituency_id);
 	echo $ok
 		? "success"
-		: "error when removing prop ballots for user_code: " . $data['active_user_code'] . ": " . $ok;
+		: "failed to remove prop ballots for user_code: " . $user_code . " and constituency: " . $constituency_id;
 }
