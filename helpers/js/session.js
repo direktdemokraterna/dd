8b83a9d710115f97652eb1364a1e550ec2b23243
joakimsigvald <joakim.sigvald@gmail.com>
2016-01-06 20:20:44 +0100

    implemented cross-domain client calls with JSONP

--- helpers/js/session.js
+++ helpers/js/session.js
@@ -1,22 +1,15 @@
-function ensureUserCodes(url, user_id, temp_code) {
-	if (sessionStorage.active_user_code ? false : true)
-		retrieveUserCodes(url, user_id, temp_code);
+var session_too_old_message = '';
+var is_session_active = false;
+
+function set_user_codes(user_codes) {
+	sessionStorage.active_user_code = user_codes.active_user_code;
+	sessionStorage.passive_user_code1 = user_codes.passive_user_code1;
+	sessionStorage.passive_user_code2 = user_codes.passive_user_code2;
+	sessionStorage.passive_user_code3 = user_codes.passive_user_code3;		
 }
 
-function retrieveUserCodes(url, user_id, temp_code) {
-	make_ajax_call(url + "give_out_user_codes.php?user_id=" + user_id + "&temp_code=" + temp_code,
-		function(response) {
-			if (response) {
-	        	var user_codes = JSON.parse(response);
-	        	sessionStorage.active_user_code = user_codes.active_user_code;
-				sessionStorage.passive_user_code1 = user_codes.passive_user_code1;
-				sessionStorage.passive_user_code2 = user_codes.passive_user_code2;
-				sessionStorage.passive_user_code3 = user_codes.passive_user_code3;
-			}
-			else {
-				alert(session_too_old);
-			}
-	    });
+function failed_to_set_user_codes() {
+	alert(session_too_old_message);
 }
 
 function set_language(lang) {
@@ -24,14 +17,4 @@ function set_language(lang) {
 		? '&'
 		: '?';
 	window.location.href=window.location.href + qsign + 'lang=' + lang;
-}
-
-function make_ajax_call(url, callback) {
-	var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-    		if (xmlhttp.readyState == 4 && xmlhttp.status == 200)
-    			callback(xmlhttp.responseText);
-    	}
-    xmlhttp.open("GET", url);
-    xmlhttp.send();
 }
