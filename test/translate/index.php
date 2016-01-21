03d11470bd68269b3c356a3ceb701d0bf492807f
joakimsigvald <joakim.sigvald@gmail.com>
2016-01-21 20:21:07 +0100

    Implemented framework for translation testcases
    - extract testcases from application
    - store testcases in xml
    - run all or selected testcase
    - generalized paging row
    TODO:
    - extract translation hints from application
    - extract new examples and mix with current
    - verify new examples
    - optimize: save previous run and do not re-run unless explicitly
    triggered
    - possible to hide verified tests
    - why is 7 tests failing?

--- test/translate/index.php
+++ test/translate/index.php
@@ -1,15 +1,34 @@
-<?php 
+<?php
 	include("../../init.inc");
-	$phrase = isset($_POST['phrase']) ? $_POST['phrase'] : '';
-	$hints = isset($_POST['hints']) ? $_POST['hints'] : '';
-	$translation_hints = $hints ? get_translation_hints($hints) : null;
-	$expected = isset($_POST['expected']) ? $_POST['expected'] : '';
-	$actual = _t($phrase, $translation_hints);
-	$output_class = $expected ? ($expected == $actual ? "correct" : "incorrect") : '';
-
-	function get_translation_hints($hints) {
-		$parts = explode('=>', $hints);
-		return array($parts[0] => $parts[1]);
+	include("xml_parser.inc");
+	include("testcases.inc");
+	$testcases = array();
+	$testcase = testcases::empty_testcase();
+	$testcase->from_text = isset($_POST['phrase']) ? $_POST['phrase'] : '';
+	$testcase->to_text = isset($_POST['expected']) ? $_POST['expected'] : '';
+	$testcase->hints = isset($_POST['hints']) ? $_POST['hints'] : '';
+	if (isset($_POST['translate'])) {
+		$testcase->test();
+	}
+	else if (isset($_POST['remove'])) {
+		$index = testcases::extract_test_index($_POST['remove']);
+		testcases::remove_at_index($index);
+		$testcases = testcases::load();
+	}
+	else if (isset($_POST['test'])) {
+		$index = testcases::extract_test_index($_POST['test']);
+		$testcases = testcases::load();
+		$testcase = $testcases[$index];
+		$testcase->test();
+	}
+	else if (isset($_POST['save'])) {
+		die('save');
+	}
+	else if (isset($_POST['run']) || isset($_GET['page'])) {
+		$testcases = testcases::load();
+	}
+	else if (isset($_POST['extract'])) {
+		$testcases = testcases::extract();
 	}
 ?>
 <html>
@@ -30,19 +49,33 @@
 		<h1 style="font-size: 24px; font-family: Lato, sans-serif; font-weight: 700; margin-bottom: 30px; ">Test translation</h1><br>
 		<form method="post" id="translate-form" action=""><p>
 			<label for "phrase">Fras</label>
-			<input type="text" name="phrase" id="phrase" tabindex="1" value="<?php echo $phrase; ?>">
+			<input type="text" name="phrase" id="phrase" tabindex="1" value="<?php 
+				echo $testcase->from_text; ?>">
 			<br>
 			<label for "hints">Direktiv</label>
-			<input type="text" name="hints" id="hints" tabindex="2" value="<?php echo $hints; ?>">
+			<input type="text" name="hints" id="hints" tabindex="2" value="<?php 
+				echo $testcase->hints; ?>">
 			<br>
 			<label for "expected">Förväntad översättning</label>
-			<input type="text" name="expected" id="expected" tabindex="3" value="<?php echo $expected; ?>">
+			<input type="text" name="expected" id="expected" tabindex="3" value="<?php 
+				echo $testcase->to_text; ?>">
 			<br>
 			<label for "actual">Översättning</label>
-			<input type="text" class="<?php echo $output_class; ?>" name="actual" id="actual" tabindex="4" readonly value="<?php echo $actual; ?>">
+			<input type="text" class="<?php echo $testcase->output_class(); ?>" name="actual" id="actual" tabindex="4" readonly value="<?php 
+				echo $testcase->actual; ?>">
 			<br>
-			<input type="submit" value="Översätt">
-		</p></form>
+			<input type="submit" name="translate" value="Översätt">
+			<input type="submit" name="save" value="Spara testfall">
+			<input type="submit" name="run" value="Kör testfall">
+			<br>
+			<input type="submit" name="extract" value="Extrahera från applikationen">
+<?php
+	if ($testcases) {
+		$page = isset($_GET['page']) ? $_GET['page'] : 1;
+		testcases::output($testcases, false, $page);
+	}
+?>
+	</p></form>
 	</div>
 	<script type="text/javascript" language="JavaScript">
 	window.setTimeout(function() { document.forms["translate-form"]["phrase"].focus(); },0);
