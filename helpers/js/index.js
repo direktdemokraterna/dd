a78d8cdb1f711f2978bb1563247247724b0f275a
hjorthammar <peter.iwanek@gmail.com>
2017-01-11 23:40:51 +0100

    Scroller
    Scroll to anchor javascript

--- helpers/js/index.js
+++ helpers/js/index.js
@@ -49,3 +49,21 @@ function setAllCheckedState(checkboxes, checked) {
 		if (checkboxes[i].type == 'checkbox')
 			checkboxes[i].checked = checked;
  }
+ 
+/* Scroll to anchor */ 
+ 
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
